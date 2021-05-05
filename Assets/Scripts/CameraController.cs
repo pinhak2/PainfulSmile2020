e49d2316ec3a44b74c78d65abb8e6e314c3dd0fb
pinhak2 <william.lima.schneider@gmail.com>
2021-05-05 17:14:47 -0300

    modified camera. cleanup code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,17 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public float fieldOfView;
 
-    void Start()
+    private void Update()
     {
-        
+        MoveCameraWithTarget();
     }
 
-    void Update()
+    private void MoveCameraWithTarget()
     {
         transform.position = new Vector3(target.transform.position.x, transform.transform.position.y, this.transform.position.z);
     }
