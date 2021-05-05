0f650dfe28657f0ee89119cd13537c7a817705a3
pinhak2 <william.lima.schneider@gmail.com>
2021-05-04 21:46:20 -0300

    changing project folder to avoid a bug

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody2D = rb;
-    public int a = 0;
+    public Rigidbody2D rb;
+
+    [SerializeField]
+    public float moveSpeed;
+    public float rotationPower;
+
+    public float rotationAmout, speed, direction, maxSpeed;
+
+    private Vector2 movement;
+
+    public float aux;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+       
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
     {
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            aux = moveSpeed;
+        }
+
+
         
+        rotationAmout = -Input.GetAxis("Horizontal");
+        if(speed < maxSpeed)      speed = aux * rotationPower;
+        direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
+        rb.rotation += rotationAmout * rotationPower * rb.velocity.magnitude;
+
+        rb.AddRelativeForce(Vector2.up * speed);
+        rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * rotationAmout / 2);
     }
 }
