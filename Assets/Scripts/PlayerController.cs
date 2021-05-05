1c36caf527acd69e04f453ec657f87a557a015bd
pinhak2 <william.lima.schneider@gmail.com>
2021-05-05 18:12:04 -0300

    stopping at obstacles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
 {
@@ -15,6 +16,9 @@ public class PlayerController : MonoBehaviour
 
     public float turnFactor = 25;
 
+    [SerializeField]
+    public Tilemap obstacle;
+
     private float accelerationInput = 0;
     private float steeringInput = 0;
     private float rotationAngle = 0;
@@ -23,10 +27,13 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D rb;
 
+    private Vector3 moveToPosition;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         health = maxHealth;
+        moveToPosition = Vector3.zero;
         UpdateHealthbar();
     }
 
@@ -36,12 +43,32 @@ public class PlayerController : MonoBehaviour
         UpdateHealthbar();
     }
 
-
-
     private void FixedUpdate()
     {
-        ApplyVerticalForce();
+        MoveIfWontCrash();
         RotateBoat();
+    }
+
+    private void MoveIfWontCrash()
+    {
+        Vector3Int obstacleMapTile = ObstacleInNextPosition();
+
+        if (obstacle.GetTile(obstacleMapTile) == null)
+        {
+            ApplyVerticalForce();
+        }
+        else
+        {
+            rb.velocity = Vector3.zero;
+        }
+    }
+
+    private Vector3Int ObstacleInNextPosition()
+    {
+        moveToPosition = transform.position + new Vector3(rb.velocity.x, rb.velocity.y, 0);
+        Vector3Int obstacleMapTile = obstacle.WorldToCell(moveToPosition);
+
+        return obstacleMapTile;
     }
 
     private void ApplyVerticalForce()
@@ -55,7 +82,7 @@ public class PlayerController : MonoBehaviour
     {
         rotationAngle -= steeringInput * turnFactor;
 
-        rb.MoveRotation(rotationAngle *Time.fixedDeltaTime);
+        rb.MoveRotation(rotationAngle * Time.fixedDeltaTime);
     }
 
     private void InputVector()
